3b164b724044a23dfb2f19dc658caf939c6d346b
Sixto José Romero Martínez <sixto@servicetechnologies.biz>
2024-06-06 09:23:19 -0500

    Se crea un nuevo campo

--- dbo/Tables/Municipios.sql
+++ dbo/Tables/Municipios.sql
@@ -2,6 +2,7 @@
     [Id_Municipio] INT           IDENTITY (1, 1) NOT NULL,
     [Nombre]       NVARCHAR (80) NOT NULL,
     [Provincia]    NVARCHAR (80) NOT NULL,
+    [ProvinciaDescripcion]    NVARCHAR (80) NOT NULL,
     CONSTRAINT [PK_Municipios] PRIMARY KEY CLUSTERED ([Id_Municipio] ASC)
 );
 
